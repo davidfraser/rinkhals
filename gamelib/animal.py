d6d626117b3f50b4733ba038bd608c97fcbd5139
Simon Cross <hodgestar@gmail.com>
2009-08-30 18:23:40 +0000

    Change animal.py to four space indents.
    --HG--
    extra : convert_revision : svn%3Ab4e93282-eac8-4b8b-b765-0f5d36de2b68%4029

--- gamelib/animal.py
+++ gamelib/animal.py
@@ -6,47 +6,46 @@ from pgu.vid import Sprite
 import data
 
 class Animal(Sprite):
-   """Base class for animals"""
+    """Base class for animals"""
 
-   def __init__(self, image, pos):
-       Sprite.__init__(self, image, pos)
-       self.pos = pos
+    def __init__(self, image, pos):
+        Sprite.__init__(self, image, pos)
+        self.pos = pos
 
-   def loop(self, tv, _sprite):
-       ppos = tv.tile_to_view(self.pos)
-       self.rect.x = ppos[0]
-       self.rect.y = ppos[1]
+    def loop(self, tv, _sprite):
+        ppos = tv.tile_to_view(self.pos)
+        self.rect.x = ppos[0]
+        self.rect.y = ppos[1]
 
-   def move(self, state):
-      """Given the game state, return a new position for the object"""
-      # Default is not to move
-      return self.pos
+    def move(self, state):
+        """Given the game state, return a new position for the object"""
+        # Default is not to move
+        return self.pos
 
 class Chicken(Animal):
-   """A chicken"""
+    """A chicken"""
 
-   def __init__(self, pos):
-       image = pygame.image.load(data.filepath('sprites/chkn.png'))
-       Animal.__init__(self, image, pos)
+    def __init__(self, pos):
+        image = pygame.image.load(data.filepath('sprites/chkn.png'))
+        Animal.__init__(self, image, pos)
 
-   def move(self, gameboard):
-       """A free chicken will move away from other free chickens"""
-       return self.pos
+    def move(self, gameboard):
+        """A free chicken will move away from other free chickens"""
+        return self.pos
 
 class Egg(Animal):
-   """An egg"""
+    """An egg"""
 
-   # Eggs don't move
+    # Eggs don't move
 
 class Fox(Animal):
-   """A fox"""
+    """A fox"""
 
-   def __init__(self, pos):
-       image = pygame.image.load(data.filepath('sprites/fox.png'))
-       Animal.__init__(self, image, pos)
+    def __init__(self, pos):
+        image = pygame.image.load(data.filepath('sprites/fox.png'))
+        Animal.__init__(self, image, pos)
 
-   def move(self, gameboard):
-       """Foxes will aim to move towards the closest henhouse or free
+    def move(self, gameboard):
+        """Foxes will aim to move towards the closest henhouse or free
           chicken"""
-       return self.pos
-       
+        return self.pos
