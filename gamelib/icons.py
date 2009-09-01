c9972c5bb4704b4e61a406fb3635b6c5bb858787
Neil Muller <drnlmuller@gmail.com>
2009-09-01 11:18:08 +0000

    Add icons for the menu
    --HG--
    extra : convert_revision : svn%3Ab4e93282-eac8-4b8b-b765-0f5d36de2b68%4079

---
+++ gamelib/icons.py
@@ -0,0 +1,8 @@
+"""Constant definitions for the icons we use"""
+
+from pgu.gui import Image
+
+import data
+
+KILLED_FOX = Image(data.filepath('icons/killed_fox.png'))
+CHKN_ICON = Image(data.filepath('icons/chkn.png'))
