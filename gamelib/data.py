0c070a1938c61b51a38ffe799297257a9debaddd
David Fraser <davidf@sjsoft.com>
2009-09-02 10:33:35 +0000

    Support multiple arguments using os.path.join - better to pass filepath('a', 'b') than filepath('a/b')
    --HG--
    extra : convert_revision : svn%3Ab4e93282-eac8-4b8b-b765-0f5d36de2b68%4090

--- gamelib/data.py
+++ gamelib/data.py
@@ -10,10 +10,10 @@ import os
 data_py = os.path.abspath(os.path.dirname(__file__))
 data_dir = os.path.normpath(os.path.join(data_py, '..', 'data'))
 
-def filepath(filename):
+def filepath(*filenames):
     '''Determine the path to a file in the data directory.
     '''
-    return os.path.join(data_dir, filename)
+    return os.path.join(data_dir, *filenames)
 
 def load(filename, mode='rb'):
     '''Open a file in the data directory.
