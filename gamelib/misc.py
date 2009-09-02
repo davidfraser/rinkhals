e863e2cf220edeef4962e4f06409428ad78e472e
Simon Cross <hodgestar@gmail.com>
2009-09-02 17:24:58 +0000

    Re-indent to four spaces.
    --HG--
    extra : convert_revision : svn%3Ab4e93282-eac8-4b8b-b765-0f5d36de2b68%40101

--- gamelib/misc.py
+++ gamelib/misc.py
@@ -1,31 +1,31 @@
 # Holder for misc useful classes
 
 class Position(object):
-   """2D position / vector"""
+    """2D position / vector"""
 
-   def __init__(self, x, y):
-       self.x = x
-       self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-   def to_tuple(self):
-       return self.x, self.y
+    def to_tuple(self):
+        return self.x, self.y
 
-   def dist(self, b):
-       """Gives the distance to another position"""
+    def dist(self, b):
+        """Gives the distance to another position"""
 
-       return abs(self.x - b.x) + abs(self.y - b.y)
+        return abs(self.x - b.x) + abs(self.y - b.y)
 
-   def __sub__(self, b):
-       return Position(self.x - b.x, self.y - b.y)
+    def __sub__(self, b):
+        return Position(self.x - b.x, self.y - b.y)
 
-   def __add__(self, b):
-       return Position(self.x + b.x, self.y + b.y)
+    def __add__(self, b):
+        return Position(self.x + b.x, self.y + b.y)
 
-   def left_of(self, b):
-       return self.x < b.x
+    def left_of(self, b):
+        return self.x < b.x
 
-   def right_of(self, b):
-       return self.x > b.x
+    def right_of(self, b):
+        return self.x > b.x
 
-   def __eq__(self, b):
-       return self.x == b.x and self.y == b.y
+    def __eq__(self, b):
+        return self.x == b.x and self.y == b.y
