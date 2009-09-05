e38a0c99803246cc81e9aa810db559456f3206ee
Simon Cross <hodgestar@gmail.com>
2009-09-05 23:59:56 +0000

    Release 1.0.0.
    --HG--
    extra : convert_revision : svn%3Ab4e93282-eac8-4b8b-b765-0f5d36de2b68%40359

--- gamelib/version.py
+++ gamelib/version.py
@@ -1,6 +1,6 @@
 """Operation Fox Assault Version Information"""
 
-VERSION = (1, 0, 0, 'alpha', 1)
+VERSION = (1, 0, 0, 'final', 0)
 BASE_VERSION_STR = '.'.join([str(x) for x in VERSION[:3]])
 VERSION_STR = {
     'final': BASE_VERSION_STR,
