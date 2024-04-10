bb25c199841d8287926118aaa45a0cebd7c75b16
David Fraser <david@frasergo.org>
2024-04-10 19:35:45 +0200

    Remove unnecessary files and zip into an archive Also unset environment variables after running

--- build_exe_folders.cmd
+++ build_exe_folders.cmd
@@ -31,3 +31,17 @@ xcopy /e /q /y %BASE_PYTHON_DIR%\DLLs\ %TARGET_DIR%\DLLs\
 xcopy /e /q /y venv\ %TARGET_DIR%\
 xcopy /e /q /y gamelib\ %TARGET_DIR%\gamelib\
 xcopy /e /q /y data\ %TARGET_DIR%\data\
+
+echo PHASE 4: Removing unnecessary files
+rd /s /q %TARGET_DIR%\Lib\test\
+
+echo PHASE 5: Zipping
+set TARGET_ARCHIVE=dist\%TARGET_NAME%
+python -c "import shutil ; from os import getenv as e; shutil.make_archive(e('TARGET_ARCHIVE'), 'zip', base_dir=e('TARGET_DIR'))"
+dir %TARGET_ARCHIVE%.zip
+
+set BASE_PYTHON_DIR=
+set VENV_PYTHON_DIR=
+set TARGET_ARCHIVE=
+set TARGET_NAME=
+set TARGET_DIR=
